dd8bcfbb02cc72a1ed711cc14b407a6b43d188a1
ivan <thetad963@gmail.com>
2018-03-24 20:49:26 +0300

    Fixed bug v1.1

--- src/MainPackage/Handler.java
+++ src/MainPackage/Handler.java
@@ -1,59 +1,32 @@
 package MainPackage;
 
-import java.util.ArrayList;
-
 import org.bukkit.Server;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerBedEnterEvent;
 import org.bukkit.event.player.PlayerBedLeaveEvent;
-import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerKickEvent;
 
 public class Handler implements Listener 
 {	
 	public int PlayerSpleepCount = 0;
-	public int CountPlayers = 0;
-	public ArrayList<Player> Plaeyrs;
 	
 	public Handler() 
 	{
-		Plaeyrs = new ArrayList<Player>();
 	}
 	
-	@EventHandler
-	public void Join(PlayerJoinEvent e) 
-	{	
-		Plaeyrs.add(e.getPlayer());
-		CountPlayers = Plaeyrs.size();
-	}
-	
-	@EventHandler
-	public void Leave(PlayerKickEvent e) 
-	{
-		Player player = e.getPlayer();
-		
-		if(Plaeyrs.size() > 0) 
-		{
-			if(Plaeyrs.contains(player)) 
-			{
-				Plaeyrs.remove(player);
-				CountPlayers--;
-			}
-		}
-	}
 	
 	@EventHandler
 	public void Sleeping(PlayerBedEnterEvent e) 
 	{
 		Player player = e.getPlayer();
 		Server server = player.getServer();
+		int coutPlayers=server.getMaxPlayers();
 		
 		PlayerSpleepCount++;
-		float percent = PlayerSpleepCount / CountPlayers * 100;
-		player.sendMessage("Sleep " + PlayerSpleepCount + " "+ "from" + " "+ CountPlayers);
-		if(percent > 50) 
+		float percent = PlayerSpleepCount / coutPlayers * 100;
+		player.sendMessage("Sleep " + PlayerSpleepCount + " "+ "from" + " "+ coutPlayers);
+		if(percent >= 50) 
 		{
 			player.sendMessage("Let there be light!");
 			server.dispatchCommand(server.getConsoleSender(),"time set 0");
